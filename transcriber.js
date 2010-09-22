3cd2819c6b0363f55d114c231a6f30ec69320285
Adam Young <ayoung@redhat.com>
2010-09-21 22:05:25 -0400

    draw and remove elements on mousemove events

--- transcriber.js
+++ transcriber.js
@@ -15,6 +15,7 @@ $(function() {
     var offsetTop = 0;
     var width = 1100;
 
+    var notes=[];
 
     function drawIntro(svg) {
 
@@ -28,21 +29,25 @@ $(function() {
             svg.line(g, diameter, diameter*i+top_line, width, diameter*i+top_line);
         }
         that.svg = svg;
+        that.notes = notes;
     }
+
+    $('#moveit').click(function(){
+    });
 
     $('#svgintro').svg({onLoad: drawIntro});
 
 
-     function quantize(centerY){
+     function quantize(center_y){
         var steps;
         note_y = top_line;
-        if (top_line > centerY){
-            steps =  (top_line - centerY) / (diameter/2);
+        if (top_line > center_y){
+            steps =  (top_line - center_y) / (diameter/2);
             for (i = 0; i < steps; i += 1){
                 note_y -= diameter/2;
             }
         }else{
-            steps =  (top_line - centerY) / (diameter/2)+1;
+            steps =  (top_line - center_y) / (diameter/2)+1;
 
             for (i = 0; i > steps; i -= 1){
                 note_y += diameter/2;
@@ -51,18 +56,31 @@ $(function() {
          return note_y;
     }
 
+    var last_circle;
+    $('#svgintro').mousemove(function(evt){
+        var center_y = evt.layerY-this.offsetTop;
+        var center_x=evt.layerX-this.offsetLeft;
+        if (last_circle){
+            that.svg.remove(last_circle);
+        }
+
+
+        last_circle =   that.svg.circle(center_x, center_y, radius,
+                        {fill: 'none', stroke: 'black', strokeWidth: 3});
+    });
+
     $('#svgintro').click(function(evt){
         div = this;
         var g = that.svg.group({stroke: 'black', strokeWidth: 2});
-        var centerY = evt.layerY-this.offsetTop;
+        var center_y = evt.layerY-this.offsetTop;
         var center_x=evt.layerX-this.offsetLeft;
         var i;
         //for now, short circuit the function.  Later, we'll use this test to decide if we are adding new notes or adjusting previous.
         if (next_note > center_x) return;
 
-        note_y =quantize(centerY);
+        note_y =quantize(center_y);
 
-        that.svg.circle(next_note, note_y, radius,
+        that.svg.circle(g,next_note, note_y, radius,
                         {fill: 'none', stroke: 'black', strokeWidth: 3});
 
         if (note_y < top_line){
@@ -76,8 +94,8 @@ $(function() {
                 that.svg.line(g, next_note-(diameter/2), i, next_note+(diameter/2), i);
             }
         }
-
-
+        notes[notes.length] = g;
         next_note += 30;
+
     });
 });
