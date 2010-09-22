2baf9cd14c4bc372fca48f529f508a39eec194f4
Adam Young <ayoung@redhat.com>
2010-09-21 22:44:54 -0400

    Crosshairs, in Yellow.

--- transcriber.js
+++ transcriber.js
@@ -38,7 +38,7 @@ $(function() {
     $('#svgintro').svg({onLoad: drawIntro});
 
 
-     function quantize(center_y){
+     function quantize_y(center_y){
         var steps;
         note_y = top_line;
         if (top_line > center_y){
@@ -56,18 +56,59 @@ $(function() {
          return note_y;
     }
 
-    var last_circle;
+
+
+    function quantize_x(center_x){
+        var steps;
+        note_x = diameter;
+        if (diameter < center_x){
+            steps =  (center_x - diameter) / (diameter/2);
+            for (i = 0; i < steps; i += 1){
+                note_x += diameter/2;
+            }
+        }else{
+            note_x = diameter;
+        }
+         return note_x;
+    }
+
+
+    var last_horiz;
+    var last_vert;
+
+    function redo_vert(center_x){
+        if (last_vert){
+            that.svg.remove(last_vert);
+            last_vert = null;
+        }
+        if ( center_x >= next_note){
+            last_vert = that.svg.line( next_note, 0 , next_note, 800,
+                                      {stroke: 'yellow', strokeWidth: 4});
+        }else{
+            center_x = quantize_x(center_x);
+            last_vert = that.svg.line( center_x, 0 , center_x, 800,
+                                       {stroke: 'yellow', strokeWidth: 4});
+        }
+    }
+
+    function redo_horiz(center_y){
+        if (last_horiz){
+            that.svg.remove(last_horiz);
+            last_horiz = null;
+        }
+        var line_y =quantize_y(center_y);
+        last_horiz = that.svg.line(diameter, line_y, width, line_y,
+                                   {stroke: 'yellow', strokeWidth: 4});
+    }
+
     $('#svgintro').mousemove(function(evt){
         var center_y = evt.layerY-this.offsetTop;
         var center_x=evt.layerX-this.offsetLeft;
-        if (last_circle){
-            that.svg.remove(last_circle);
-        }
-
-
-        last_circle =   that.svg.circle(center_x, center_y, radius,
-                        {fill: 'none', stroke: 'black', strokeWidth: 3});
+        redo_vert(center_x);
+        redo_horiz(center_y);
     });
+
+
 
     $('#svgintro').click(function(evt){
         div = this;
@@ -78,7 +119,9 @@ $(function() {
         //for now, short circuit the function.  Later, we'll use this test to decide if we are adding new notes or adjusting previous.
         if (next_note > center_x) return;
 
-        note_y =quantize(center_y);
+        note_y =quantize_y(center_y);
+
+        redo_vert(center_x);
 
         that.svg.circle(g,next_note, note_y, radius,
                         {fill: 'none', stroke: 'black', strokeWidth: 3});
