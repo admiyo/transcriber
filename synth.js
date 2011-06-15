6efc8802c155fa1397c485f94bef8fb669a96b6f
Adam Young <ayoung@redhat.com>
2011-06-14 20:11:34 -0400

    renamed osc to synth

--- synth.js
+++ synth.js
@@ -14,7 +14,6 @@ function Oscillator(samplerate, freq)
 	}
 	this.phaseOffset = 0;
 	this.pulseWidth = 0.5;
-	this.waveShape = 0; // ['Sine', 'Triangle', 'Pulse', 'Sawtooth', 'Invert Sawtooth', 'Square']
 	this.samplerate = samplerate;
 	this.generate = function(/* FM1, FM2, ... FMX */){
 		var	f	= this.frequency + 0,
@@ -33,25 +32,7 @@ function Oscillator(samplerate, freq)
 		}
 	};
 	this.getMix = function(){
-		switch(this.waveShape){
-			case 1:
-				return this.triangle();
-				break;
-			case 2:
-				return this.pulse();
-				break;
-			case 3:
-				return this.sawtooth();
-				break;
-			case 4:
-				return this.invSawtooth();
-				break;
-			case 5:
-				return this.square();
-				break;
-			default:
-				return this.sine();
-		}
+	    return this.sine();
 	};
 	this.getPhase = function(){
 		return p;
@@ -105,6 +86,9 @@ function Oscillator(samplerate, freq)
 function Synthesizer(){
 
 var freqs ={ a : 220.00  , b :  246.94  , c :  261.63  , d : 293.66 , e : 329.63, f: 349.23 , g : 392.00  };
+
+var frequencies = [
+    16.352,17.324,18.354,19.445,20.602,21.827,23.125,24.500,25.957,27.500,29.135,30.868,32.703,34.648,36.708,38.891,41.203,43.654,46.249,48.999,51.913,55.000,58.270,61.735,65.406,69.296,73.416,77.782,82.407,87.307,92.499,97.999,103.83,110.00,116.54,123.47,130.81,138.59,146.83,155.56,164.81,174.61,185.00,196.00,207.65,220.00,233.08,246.94,261.63,277.18,293.66,311.13,329.63,349.23,369.99,392.00,415.30,440.00,466.16,493.88,523.25,554.37,587.33,622.25,659.26,698.46,739.99,783.99,830.61,880.00,932.33,987.77,1046.5,1108.7,1174.7,1244.5,1318.5,1396.9,1480.0,1568.0,1661.2,1760.0,1864.7,1975.5,2093.0,2217.5,2349.3,2489.0,2637.0,2793.8,2960.0,3136.0,3322.4,3520.0,3729.3,3951.1,4186.0,4434.9,4698.6,4978.0,5274.0,5587.7,5919.9,6271.9,6644.9,7040.0,7458.6,7902.1,8372.0,8869.8,9397.3,9956.1,10548.1,11175.3,11839.8,12543.9,13289.8,14080.0,14917.2,15804.3,16744.0,17739.7,18794.5,19912.1,21096.2,22350.6,23679.6,25087.7,26579.5,28160.0,29834.5,31608.5];
 
 
 
